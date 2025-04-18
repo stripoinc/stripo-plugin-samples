d8eb05050c5cdf832c4ec5dceab91a535a556986
antongrinenko <anton.grinenko@gmail.com>
2025-04-18 13:18:13 +0300

    Updated extensions example

--- extensions/01-hello-world/src/const.js
+++ extensions/01-hello-world/src/const.js
@@ -1,4 +1,4 @@
-export const PLUGIN_ID = '127b225ce5b946a5b46ef05b1a241267';
-export const SECRET_KEY = 'cecc40a9939f464eabb4d0f0b394a0a4';
+export const PLUGIN_ID = 'YOUR_PLUGIN_ID';
+export const SECRET_KEY = 'YOUR_SECRET_KEY';
 
 export const EDITOR_URL = 'https://dev-account.stripo.email/resources/coediting/UIEditor.js';
