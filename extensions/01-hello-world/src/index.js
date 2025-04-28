ebef252cae947897a213f2ae6825e17659af27e2
antongrinenko <anton.grinenko@gmail.com>
2025-04-28 13:11:10 +0300

    Renamed extension sample

--- extensions/01-hello-world/src/index.js
+++ extensions/01-hello-world/src/index.js
@@ -1,27 +1,4 @@
-import {ExtensionBuilder} from '@stripoinc/ui-editor-extensions';
 import {initPlugin} from './StripoLoader';
-import {HelloWorldBlock} from './blocks/HelloWorldBlock';
-import en from './i18n/en';
-import uk from './i18n/uk';
-import {HelloWorldBlockContextAction} from './blocks/HelloWorldBlockContextAction';
-import {HelloWorldBlockSettingsPanelRegistry} from './blocks/HelloWorldBlockSettingsPanelRegistry';
-import {HelloWorldBackgroundColorControl} from './controls/HelloWorldBackgroundColorControl';
-import styles from './styles/extension-styles.css?raw';
-import {BrandColorPickerUIElement} from './ui-elements/BrandColorPickerUIElement';
-import {BuildInUIElementsDemoControl} from './controls/BuildInUIElementsDemoControl';
-
-const extension = new ExtensionBuilder()
-    .withLocalization({
-        'en': en,
-        'uk': uk,
-    })
-    .withStyles(styles)
-    .addBlock(HelloWorldBlock)
-    .addContextAction(HelloWorldBlockContextAction)
-    .addControl(HelloWorldBackgroundColorControl)
-    .addControl(BuildInUIElementsDemoControl)
-    .addUiElement(BrandColorPickerUIElement)
-    .withSettingsPanelRegistry(HelloWorldBlockSettingsPanelRegistry)
-    .build();
+import extension from './extension';
 
 initPlugin(extension);
