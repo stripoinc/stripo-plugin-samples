e4d9348d2d209cc94680054eaa99a11c9dcb5b3f
Igor Gripas <igor.gripas@stripo.email>
2025-04-29 09:23:42 +0300

    Update const.js

--- extensions/external-merge-tags/src/const.js
+++ extensions/external-merge-tags/src/const.js
@@ -1,5 +1,4 @@
-export const PLUGIN_ID = '38012073c8154cd2b0b6229ef558bfa4';
-export const SECRET_KEY = 'b079add0b6ba4272bb7cb07de42d8cf1';
+export const PLUGIN_ID = 'PLUGIN_ID';
+export const SECRET_KEY = 'SECRET_KEY';
 
-// export const EDITOR_URL = 'https://dev-account.stripo.email/resources/coediting/UIEditor.js';
-export const EDITOR_URL = 'http://localhost:5010/UIEditor.js';
+export const EDITOR_URL = 'https://dev-account.stripo.email/resources/coediting/UIEditor.js';
